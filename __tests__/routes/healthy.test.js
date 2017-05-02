4466fc1f7a42d707a0b45deb9b351a5b80df4727
Alexander Katsero <alexanderkatsero@gmail.com>
2017-05-02 12:49:47 +0300

    testing

---
+++ __tests__/routes/healthy.test.js
@@ -0,0 +1,18 @@
+/* global describe it */
+import { expect } from 'chai'
+
+import server from '../../../__mocks__/utils/server.mock'
+
+const ENDPOINT = '/healthy'
+
+describe('GET' + ENDPOINT, () => {
+  it('should return the _id of the user and a token', done => {
+    server.get(ENDPOINT).end((err, res) => {
+      const { status, body } = res
+      expect(err).to.be.null
+      expect(status).to.equal(200)
+      expect(body).to.haveOwnProperty('message')
+      done()
+    })
+  })
+})
